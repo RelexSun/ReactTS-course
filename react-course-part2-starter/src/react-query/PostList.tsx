683390ed56557b2cac9bd2d7dd39e00731be3c63
RelexSun <88622289+RelexSun@users.noreply.github.com>
2024-01-29 15:28:04 +0700

    infinite query

--- react-course-part2-starter/src/react-query/PostList.tsx
+++ react-course-part2-starter/src/react-query/PostList.tsx
@@ -1,11 +1,19 @@
+import React from "react";
 import { useState } from "react";
 import usePosts from "./hooks/usePosts";
 
 const PostList = () => {
   // const [userId, setUserId] = useState<number>();
   const pageSize = 10;
-  const [page, setPage] = useState(1);
-  const { data: posts, error, isLoading } = usePosts({ page, pageSize });
+  // const [page, setPage] = useState(1);
+  // const { data: posts, error, isLoading } = usePosts({ page, pageSize });
+  const {
+    data: posts,
+    error,
+    isLoading,
+    fetchNextPage,
+    isFetchingNextPage,
+  } = usePosts({ pageSize });
 
   if (isLoading) return <p>Loading...</p>;
   if (error) return <p>{error.message}</p>;
@@ -23,24 +31,34 @@ const PostList = () => {
         <option value="3">User 3</option>
       </select> */}
       <ul className="list-group">
-        {posts?.map((post) => (
+        {posts.pages.map((page, index) => (
+          <React.Fragment key={index}>
+            {page.map((post) => (
+              <li key={post.id} className="list-group-item">
+                {post.title}
+              </li>
+            ))}
+          </React.Fragment>
+        ))}
+        {/* {posts.map((post) => (
           <li key={post.id} className="list-group-item">
             {post.title}
           </li>
-        ))}
+        ))} */}
       </ul>
-      <button
+      {/* <button
         disabled={page === 1}
         onClick={() => setPage(page - 1)}
         className="btn btn-primary my-3"
       >
         Prev
-      </button>
+      </button> */}
       <button
-        onClick={() => setPage(page + 1)}
+        onClick={() => fetchNextPage()}
+        disabled={isFetchingNextPage}
         className="btn btn-primary my-3 ms-1"
       >
-        Next
+        {isFetchingNextPage ? "Loading" : "Load More"}
       </button>
     </>
   );
--- react-course-part2-starter/src/react-query/hooks/usePosts.tsx
+++ react-course-part2-starter/src/react-query/hooks/usePosts.tsx
@@ -1,4 +1,4 @@
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
 import axios from "axios";
 
 interface Post {
@@ -9,27 +9,29 @@ interface Post {
 }
 
 interface PostQuery {
-  page: number;
+  // page: number;
   pageSize: number;
 }
 
 const usePosts = (query: PostQuery) => {
-  const fetchData = () =>
-    axios
-      .get("https://jsonplaceholder.typicode.com/posts", {
-        params: {
-          // userId,
-          _start: (query.page - 1) * query.pageSize,
-          _limit: query.pageSize,
-        },
-      })
-      .then((res) => res.data);
-  return useQuery<Post[], Error>({
+  return useInfiniteQuery<Post[], Error>({
     queryKey: ["posts", query],
     // queryKey: userId ? ["users", userId, "posts"] : ["posts"],
-    queryFn: fetchData,
+    queryFn: ({ pageParam = 1 }) =>
+      axios
+        .get("https://jsonplaceholder.typicode.com/posts", {
+          params: {
+            // userId,
+            _start: (pageParam - 1) * query.pageSize,
+            _limit: query.pageSize,
+          },
+        })
+        .then((res) => res.data),
     staleTime: 1 * 60 * 1000, // 1m
     keepPreviousData: true,
+    getNextPageParam: (lastPage, allPages) => {
+      return lastPage.length > 0 ? allPages.length + 1 : undefined;
+    },
   });
 };
 
