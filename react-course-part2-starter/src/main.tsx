ac4bb8a5128cc585c874c1f43f457d68a22123b8
RelexSun <88622289+RelexSun@users.noreply.github.com>
2024-01-29 15:02:56 +0700

    parameterized Queries

--- react-course-part2-starter/src/main.tsx
+++ react-course-part2-starter/src/main.tsx
@@ -1,16 +1,33 @@
 import "bootstrap/dist/css/bootstrap.css";
 import React from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+// const queryClient = new QueryClient({
+//   defaultOptions: {
+//     queries: {
+//       // retry 3 times when there's a problem occures while fetching
+//       retry: 3,
+
+//       // if no component is using that query in 5m it'll be removed
+//       cacheTime: 300_000, //5m
+//       staleTime: 0, // auto refresh
+//       refetchOnWindowFocus: false,
+//       refetchOnReconnect: false,
+//       refetchOnMount: false,
+//     },
+//   },
+// });
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
+      <ReactQueryDevtools />
     </QueryClientProvider>
   </React.StrictMode>
 );
