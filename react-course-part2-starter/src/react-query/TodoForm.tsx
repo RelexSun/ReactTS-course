0dcdd2e99f80e669de5e2f48a997e4f7ba2a524f
RelexSun <88622289+RelexSun@users.noreply.github.com>
2024-02-01 09:02:27 +0700

    optimizing updates

--- react-course-part2-starter/src/react-query/TodoForm.tsx
+++ react-course-part2-starter/src/react-query/TodoForm.tsx
@@ -3,27 +3,43 @@ import { useRef } from "react";
 import { Todo } from "./hooks/useTodos";
 import axios from "axios";
 
+interface AddTodoContext {
+  prevTodo: Todo[];
+}
 const TodoForm = () => {
   const queryClient = useQueryClient();
   // <Todo, Error, Todo> = <data from backend, error, data to backend>
-  const addTodo = useMutation<Todo, Error, Todo>({
+  const addTodo = useMutation<Todo, Error, Todo, AddTodoContext>({
     mutationFn: (todo: Todo) =>
       axios
         .post<Todo>("https://jsonplaceholder.typicode.com/todos", todo)
         .then((res) => res.data),
-    onSuccess: (savedTodo, newTodo) => {
-      // APPROACH: Invalidating the cache
-      // queryClient.invalidateQueries({
-      //   queryKey: ["todos"]
-      // });
-      // APPROACH 2: Updating the data in the cache
+
+    onMutate: (newTodo: Todo) => {
+      const prevTodo = queryClient.getQueryData<Todo[]>(["todos"]) || [];
+
+      // Updating the data in the cache
       queryClient.setQueryData<Todo[]>(["todos"], (todos) => [
-        savedTodo,
+        newTodo,
         ...(todos || []),
       ]);
       if (ref.current) ref.current.value = "";
+
+      return { prevTodo };
+    },
+    onSuccess: (savedTodo, newTodo) => {
+      queryClient.setQueryData<Todo[]>(["todos"], (todos) =>
+        todos?.map((todo) => (todo === newTodo ? savedTodo : todo))
+      );
+    },
+
+    onError: (error, newTodo, context) => {
+      if (!context) return;
+
+      queryClient.setQueryData<Todo[]>(["todos"], context.prevTodo);
     },
   });
+
   const ref = useRef<HTMLInputElement>(null);
 
   return (
