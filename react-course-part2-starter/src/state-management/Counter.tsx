e22fbbe7f030b313a3654cd03b435f0d4ad87283
RelexSun <88622289+RelexSun@users.noreply.github.com>
2024-02-09 22:11:28 +0700

    Consolidating State Logic with a Reducer

--- react-course-part2-starter/src/state-management/Counter.tsx
+++ react-course-part2-starter/src/state-management/Counter.tsx
@@ -1,19 +1,23 @@
-import { useState } from 'react';
+import { useState, useReducer } from "react";
+import CounterReducer from "./reducers/CounterReducer";
 
 const Counter = () => {
-  const [value, setValue] = useState(0);
+  // const [value, setValue] = useState(0);
+  const [value, dispatch] = useReducer(CounterReducer, 0);
+  // Dispatch = setValue
 
   return (
     <div>
       Counter ({value})
       <button
-        onClick={() => setValue(value + 1)}
+        // onClick={() => setValue(value + 1)}
+        onClick={() => dispatch({ type: "INCREMENT" })}
         className="btn btn-primary mx-1"
       >
         Increment
       </button>
       <button
-        onClick={() => setValue(0)}
+        onClick={() => dispatch({ type: "RESET" })}
         className="btn btn-primary mx-1"
       >
         Reset
