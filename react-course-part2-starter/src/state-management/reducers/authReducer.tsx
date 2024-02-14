50638a2dc24d74b6b4998475221043094c761f8f
RelexSun <88622289+RelexSun@users.noreply.github.com>
2024-02-14 21:21:13 +0700

    creating complex actions + working with reducers

---
+++ react-course-part2-starter/src/state-management/reducers/authReducer.tsx
@@ -0,0 +1,19 @@
+interface LoginAction {
+  type: "LOGIN";
+  username: string;
+}
+
+interface LogoutAction {
+  type: "LOGOUT";
+}
+
+type AuthAction = LoginAction | LogoutAction;
+
+const authReducer = (state: string, action: AuthAction): string => {
+  if (action.type === "LOGIN") return action.username;
+  // Should explicitly ues if statement for app to be scalability (reset password...)
+  if (action.type === "LOGOUT") return "";
+  return state;
+};
+
+export default authReducer;
---
+++ react-course-part2-starter/src/state-management/reducers/tasksReducer.tsx
@@ -0,0 +1,28 @@
+interface Task {
+  id: number;
+  title: string;
+}
+
+interface AddTask {
+  type: "ADD";
+  task: Task;
+}
+
+interface DelteTask {
+  type: "DELETE";
+  taskId: number;
+}
+
+type TaskAction = AddTask | DelteTask;
+
+const tasksReducer = (tasks: Task[], action: TaskAction): Task[] => {
+  // TYPE SAFTEY: TS automatically knows what type we should use when action + (.)
+  switch (action.type) {
+    case "ADD":
+      return [action.task, ...tasks];
+    case "DELETE":
+      return tasks.filter((t) => t.id !== action.taskId);
+  }
+};
+
+export default tasksReducer;
