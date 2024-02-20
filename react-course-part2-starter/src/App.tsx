44022e9e10c6eb6cf71f59373db1523d97cc0709
RelexSun <88622289+RelexSun@users.noreply.github.com>
2024-02-20 21:43:25 +0700

    organizing code for scalability and maintainability

--- react-course-part2-starter/src/App.tsx
+++ react-course-part2-starter/src/App.tsx
@@ -1,19 +1,8 @@
-import { useReducer } from "react";
 import "./App.css";
-import PostList from "./react-query/PostList";
-import TodoForm from "./react-query/TodoForm";
-import TodoList from "./react-query/TodoList";
-import Counter from "./state-management/Counter";
-import LoginStatus from "./state-management/LoginStatus";
-import TaskList from "./state-management/TaskList";
-import tasksReducer from "./state-management/reducers/tasksReducer";
-import TaskContext from "./state-management/contexts/taskContext";
-import NavBar from "./state-management/NavBar";
-import HomePage from "./state-management/HomePage";
-import authReducer from "./state-management/reducers/authReducer";
-import AuthContext from "./state-management/contexts/authContext";
 import AuthProvider from "./state-management/AuthProvider";
-import TaskProvider from "./state-management/TaskProvider";
+import HomePage from "./state-management/HomePage";
+import NavBar from "./state-management/NavBar";
+import { TaskProvider } from "./state-management/tasks";
 
 function App() {
   return (
