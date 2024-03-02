7aaca48400c1531c2be459a76706f91bf8f054e5
RelexSun <88622289+RelexSun@users.noreply.github.com>
2024-03-02 11:13:46 +0700

    setting up routing

--- react-course-part2-starter/src/main.tsx
+++ react-course-part2-starter/src/main.tsx
@@ -5,14 +5,16 @@ import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import router from "./routing/routes";
 
 // const queryClient = new QueryClient({
 //   defaultOptions: {
 //     queries: {
-//       // retry 3 times when there's a problem occures while fetching
+// retry 3 times when there's a problem occures while fetching
 //       retry: 3,
 
-//       // if no component is using that query in 5m it'll be removed
+// if no component is using that query in 5m it'll be removed
 //       cacheTime: 300_000, //5m
 //       staleTime: 0, // auto refresh
 //       refetchOnWindowFocus: false,
@@ -26,7 +28,7 @@ const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <RouterProvider router={router} />
       <ReactQueryDevtools />
     </QueryClientProvider>
   </React.StrictMode>
