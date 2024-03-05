69cfbdb6222c5ccaefa7296902c2c04e3e28044b
RelexSun <88622289+RelexSun@users.noreply.github.com>
2024-03-05 21:04:45 +0700

    nested routes

--- react-course-part2-starter/src/routing/routes.tsx
+++ react-course-part2-starter/src/routing/routes.tsx
@@ -3,11 +3,20 @@ import HomePage from "./HomePage";
 import UserListPage from "./UserListPage";
 import ContactPage from "./ContactPage";
 import UserDetailPage from "./UserDetailPage";
+import Layout from "./Layout";
 
 const router = createBrowserRouter([
-  { path: "/", element: <HomePage /> },
-  { path: "/users", element: <UserListPage /> },
-  { path: "/users/:id", element: <UserDetailPage /> },
+  {
+    path: "/",
+    element: <Layout />,
+    // the children route is already a relative to the layout so we dont need to use '/'
+    children: [
+      // index to true is also the same as path '/' or path ""
+      { index: true, element: <HomePage /> },
+      { path: "users", element: <UserListPage /> },
+      { path: "users/:id", element: <UserDetailPage /> },
+    ],
+  },
 ]);
 
 export default router;
